686b48906b211107df3462f0f9363e6389f510c1
kou5321 <757330477@qq.com>
2022-02-03 23:31:20 +0800

    add 448

---
+++ Array/findDisappearNumber.java
@@ -0,0 +1,21 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class findDisappearNumber {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int n = nums.length;
+        for (int num : nums) {
+            int x = (num - 1) % n;
+            nums[x] += n;
+        }
+        List<Integer> result = new ArrayList<Integer>();
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= n) {
+                result.add(i+1);
+            }
+        }
+        return result;
+    }
+}
