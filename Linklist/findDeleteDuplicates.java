c6d7eb83b42475066102bf8e7a2777688767bf3d
kou5321 <757330477@qq.com>
2022-02-04 21:42:50 +0800

    add 83

---
+++ Linklist/findDeleteDuplicates.java
@@ -0,0 +1,29 @@
+package Linklist;
+
+public class findDeleteDuplicates {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode cur = head;
+        while (cur.next != null) { // 注意是next
+            if (cur.next.val == cur.val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return head;
+    }
+}
