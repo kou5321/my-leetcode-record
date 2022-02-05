6b580b05b1d0a73c0c4d1a1edc9e4a0b7e5e2a2a
kou5321 <757330477@qq.com>
2022-02-05 22:28:01 +0800

    add 142

---
+++ Linklist/findDetectCycle.java
@@ -0,0 +1,24 @@
+package Linklist;
+
+public class findDetectCycle {
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast, slow;
+        fast = slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                break;
+            }
+        }
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
