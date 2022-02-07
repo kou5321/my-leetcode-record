930a363b7fd62a268a17047a2aed7dba099516d3
kou5321 <757330477@qq.com>
2022-02-07 22:05:22 +0800

    add 234

---
+++ Linklist/findIsPalidrome.java
@@ -0,0 +1,34 @@
+package Linklist;
+
+public class findIsPalidrome {
+    public boolean isPalindrome(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        if (fast != null) {
+            slow = slow.next;
+        }
+        ListNode left = head;
+        ListNode right = reverse(slow);
+        while (right != null) {
+            if (left.val != right.val) {
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+        return true;
+    }
+    ListNode reverse(ListNode head) {
+        ListNode pre = null, cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+}
