8d82aec3a8b38f4c0b5bc96e4d6bcef9c904a9ab
kou5321 <757330477@qq.com>
2022-02-16 23:04:57 +0800

    add 104

---
+++ Tree/findMaxDepth.java
@@ -0,0 +1,10 @@
+package Tree;
+
+import javax.swing.tree.TreeNode;
+
+public class findMaxDepth {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
