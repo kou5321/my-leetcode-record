883e15015d9629b184c60b816097353c8c6aba86
kou5321 <757330477@qq.com>
2022-02-16 22:57:02 +0800

    add 27

---
+++ Tree/findSymmetricTree.java
@@ -0,0 +1,23 @@
+package Tree;
+
+import javax.swing.tree.TreeNode;
+
+public class findSymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return deepCheck(root.left, root.right);
+    }
+
+    public boolean deepCheck(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return deepCheck(left.left, right.right) && deepCheck(left.right, right.left);
+    }
+}
