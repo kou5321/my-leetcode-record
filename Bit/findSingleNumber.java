052b0c7a2251e4dcb55ca0855d97efe194b7c62a
kou5321 <757330477@qq.com>
2022-02-22 21:22:26 +0800

    add 136

---
+++ Bit/findSingleNumber.java
@@ -0,0 +1,11 @@
+package Bit;
+
+public class findSingleNumber {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result = result ^ num;
+        }
+        return result;
+    }
+}
