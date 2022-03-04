da82cb15e43dbfc1d830723c4a0f0cb424ee7e9b
kou5321 <757330477@qq.com>
2022-03-04 10:36:00 +0800

    add 27

---
+++ Array/RemoveElement27.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class RemoveElement27 {
+    public int removeElement(int[] nums, int val) {
+        int fast = 0, slow = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow;
+    }
+}
