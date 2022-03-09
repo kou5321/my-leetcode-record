649c4e8953cdba0bcd2ef77d9b22770227815c96
kou5321 <757330477@qq.com>
2022-03-09 16:18:25 +0800

    add 450

---
+++ Tree/deleteNodeInBST450.java
@@ -0,0 +1,30 @@
+package Tree;
+
+import javax.swing.tree.TreeNode;
+
+public class deleteNodeInBST450 {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) return null;
+        if (root.val == key) {
+            if (root.left == null) return root.right;
+            if (root.right == null) return root.left;
+            TreeNode minNode = getMin(root.right);
+            root.right = deleteNode(root.right, minNode.val);
+            minNode.left = root.left;
+            minNode.right = root.right;
+            root = minNode;
+        } else if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+        } else if (root.val < key) {
+            root.right = deleteNode(root.right, key);
+        }
+        return root;
+    }
+
+    TreeNode getMin(TreeNode root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
+}
