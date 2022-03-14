181832590ce25bf7ecaf7db58394c7d59b6f58b4
kou5321 <757330477@qq.com>
2022-03-14 16:07:28 +0800

    add 1254

---
+++ ExhaustiveSearch/numberOfClosedIslands1254.java
@@ -0,0 +1,41 @@
+package ExhaustiveSearch;
+
+public class numberOfClosedIslands1254 {
+    public int closedIsland(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int res = 0;
+        for (int j = 0; j < n; j++) {
+            // 把靠上边的岛屿淹掉
+            dfs(grid, 0, j);
+            // 把靠下边的岛屿淹掉
+            dfs(grid, m - 1, j);
+        }
+
+        for (int i = 0; i < m; i++) {
+            // 把靠左边的岛屿淹掉
+            dfs(grid, i, 0);
+            // 把靠右边的岛屿淹掉
+            dfs(grid, i, n - 1);
+        }
+        // 遍历 grid，剩下的岛屿都是封闭岛屿
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 0) {
+                    res++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return res;
+    }
+
+    public void dfs(int[][] grid, int i, int j) {
+        // 注意是大于等于
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length) return;
+        if (grid[i][j] == 1) return;
+        grid[i][j] = 1;
+        dfs(grid, i+1, j);
+        dfs(grid, i-1, j);
+        dfs(grid, i, j-1);
+        dfs(grid, i, j+1);
+}
