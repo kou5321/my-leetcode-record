dd4ad35c750825631f7b67bcba48c6f25aaafc2f
kou5321 <757330477@qq.com>
2022-03-15 14:46:57 +0800

    add 200

---
+++ ExhaustiveSearch/numberOfEnclaves200.java
@@ -0,0 +1,36 @@
+package ExhaustiveSearch;
+
+public class numberOfEnclaves200 {
+    public int numEnclaves(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int res = 0;
+        for (int i = 0; i < m; i++) {
+            dfs(grid, i , 0);
+            dfs(grid, i, n-1);
+        }
+        for (int j = 0; j < n; j++) {
+            dfs(grid, 0 , j);
+            dfs(grid, m-1, j);
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    res += 1;
+                }
+            }
+        }
+        return res;
+    }
+
+    public void dfs(int[][] grid, int i, int j) {
+        int m = grid.length;
+        int n = grid[0].length;
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0) return;
+        grid[i][j] = 0;
+        dfs(grid, i+1, j);
+        dfs(grid, i-1, j);
+        dfs(grid, i, j+1);
+        dfs(grid, i, j-1);
+    }
+}
