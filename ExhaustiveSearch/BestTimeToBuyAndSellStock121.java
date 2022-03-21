132b1a8f3498b6891b792fb9b6da803194ecc76b
kou5321 <757330477@qq.com>
2022-03-21 15:08:02 +0800

    add 122

--- ExhaustiveSearch/BestTimeToBuyAndSellStock121.java
+++ ExhaustiveSearch/BestTimeToBuyAndSellStock121.java
@@ -1,7 +1,7 @@
 package ExhaustiveSearch;
 
 public class BestTimeToBuyAndSellStock121 {
-    public int maxProfit(int[] prices) {
+    public static int maxProfit(int[] prices) {
         int[][] dp = new int[prices.length][2];
         for (int i = 0; i < prices.length; i++) {
             if (i - 1 == -1) {
@@ -14,5 +14,10 @@ public class BestTimeToBuyAndSellStock121 {
             dp[i][1] = Math.max(dp[i-1][1], -prices[i]); // don't use dp[i-1][0]
         }
         return dp[prices.length-1][0];
+    }
+
+    public static void main(String[] args) {
+        int []arr = {7,1,5,3,6,4};
+        System.out.println(maxProfit(arr));
     }
 }
