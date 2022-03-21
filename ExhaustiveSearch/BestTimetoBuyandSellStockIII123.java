e933a3c642b5e8dc7638446cd0a456bbb0112fa9
kou5321 <757330477@qq.com>
2022-03-21 15:50:29 +0800

    add 123

---
+++ ExhaustiveSearch/BestTimetoBuyandSellStockIII123.java
@@ -0,0 +1,20 @@
+package ExhaustiveSearch;
+
+public class BestTimetoBuyandSellStockIII123 {
+    public int maxProfit(int[] prices) {
+        int max_k = 2, n = prices.length;
+        int[][][] dp = new int[n][max_k+1][2]; // +1
+        for (int i = 0; i < n; i++) {
+            for (int k = max_k; k >= 1; k--) {
+                if (i - 1 == -1) {
+                    dp[i][k][0] = 0;
+                    dp[i][k][1] = -prices[i];
+                    continue;
+                }
+                dp[i][k][0] = Math.max(dp[i-1][k][0], dp[i-1][k][1]+prices[i]);
+                dp[i][k][1] = Math.max(dp[i-1][k][1], dp[i-1][k-1][0]-prices[i]);
+            }
+        }
+        return dp[n-1][max_k][0];
+    }
+}
