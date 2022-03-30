22401873766f3befa45c8dfd03e1112b28277d13
kou5321 <757330477@qq.com>
2022-03-30 16:16:51 +0800

    add 35

---
+++ Search/SearchInsertPosition35.java
@@ -0,0 +1,22 @@
+package Search;
+
+public class SearchInsertPosition35 {
+    public int searchInsert(int[] nums, int target) {
+        return left_bound(nums, target);
+    }
+
+    int left_bound(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                right = mid - 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
