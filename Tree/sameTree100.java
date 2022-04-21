eca20f505c6c8657647c9f511db172aa29def161
kou5321 <757330477@qq.com>
2022-04-21 20:39:03 +0800

    add 100

---
+++ Tree/sameTree100.java
@@ -0,0 +1,18 @@
+package Tree;
+
+import javax.swing.tree.TreeNode;
+
+public class sameTree100 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
